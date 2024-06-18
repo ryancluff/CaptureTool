5853b19699f16e4411e8ca3055d84c929381378a
Ryan <ryancluf@yahoo.com>
2024-06-18 18:12:55 -0400

    simplify capture script

--- main.py
+++ main.py
@@ -13,73 +13,52 @@ if __name__ == "__main__":
         print(f"Plays a wave file. Usage: {sys.argv[0]} playback.wav record.wav")
         sys.exit(-1)
 
-    with wave.open(sys.argv[1], "rb") as play_wf:
-        with wave.open(sys.argv[2], "wb") as record_wf:
-            # Define callback for playback (1)
-            def play_callback(in_data, frame_count, time_info, status):
-                data = play_wf.readframes(frame_count)
-                # If len(data) is less than requested frame_count, PyAudio automatically
-                # assumes the stream is finished, and the stream stops.
-                return (data, pyaudio.paContinue)
-            
-            # Instantiate PyAudio and initialize PortAudio system resources
-            p = pyaudio.PyAudio()
+    play_wf = wave.open(sys.argv[1], "rb")
+    record_wf = wave.open(sys.argv[2], "wb")
 
-            # # Get the host APIs
-            host_api_count = p.get_host_api_count()
-            for i in range(host_api_count):
-                host_api_info = p.get_host_api_info_by_index(i)
-                print(f'Host API {i}: {host_api_info["name"]}')
+    # Define callback for playback (1)
+    def play_callback(in_data, frame_count, time_info, status):
+        data = play_wf.readframes(frame_count)
+        # If len(data) is less than requested frame_count, PyAudio automatically
+        # assumes the stream is finished, and the stream stops.
+        return (data, pyaudio.paContinue)
+    
+    # Instantiate PyAudio and initialize PortAudio system resources
+    p = pyaudio.PyAudio()
 
-            # Get the devices
-            device_count = p.get_device_count()
-            for i in range(device_count):
-                device_info = p.get_device_info_by_index(i)
-                print(f'Device {i}: {device_info["name"]}')
-                print(f'  Input channels: {device_info["maxInputChannels"]}')
-                print(f'  Output channels: {device_info["maxOutputChannels"]}')
-            print("\n")
+    record_wf.setnchannels(play_wf.getnchannels())
+    record_wf.setsampwidth(play_wf.getsampwidth())
+    record_wf.setframerate(play_wf.getframerate())
 
-            # Get the defaults
-            host_api_info = p.get_default_host_api_info()
-            output_device_info = p.get_default_output_device_info()
-            input_device_info = p.get_default_input_device_info()
-            print(f'Default host API: {host_api_info["name"]}')
-            print(f'Default output device: {output_device_info["name"]}')
-            print(f'Default input device: {input_device_info["name"]}')
+    record_stream = p.open(
+        format=p.get_format_from_width(play_wf.getsampwidth()),
+        channels=play_wf.getnchannels(),
+        rate=play_wf.getframerate(),
+        input=True,
+    )
 
-            record_wf.setnchannels(play_wf.getnchannels())
-            record_wf.setsampwidth(play_wf.getsampwidth())
-            record_wf.setframerate(play_wf.getframerate())
+    play_stream = p.open(
+        format=p.get_format_from_width(play_wf.getsampwidth()),
+        channels=play_wf.getnchannels(),
+        rate=play_wf.getframerate(),
+        output=True,
+        stream_callback=play_callback,
+    )
 
-            record_stream = p.open(
-                format=p.get_format_from_width(play_wf.getsampwidth()),
-                channels=play_wf.getnchannels(),
-                rate=play_wf.getframerate(),
-                input=True,
-            )
+    # Wait for stream to finish
+    print('Recording...')
+    while play_stream.is_active():
+        data = record_stream.read(128)
+        record_wf.writeframes(data)
+    print('Done')
 
-            play_stream = p.open(
-                format=p.get_format_from_width(play_wf.getsampwidth()),
-                channels=play_wf.getnchannels(),
-                rate=play_wf.getframerate(),
-                output=True,
-                stream_callback=play_callback,
-            )
+    # Close the streams
+    record_stream.close()
+    play_stream.close()
 
-            # Wait for stream to finish
-            print('Recording...')
-            while play_stream.is_active():
-                data = record_stream.read(128)
+    # Close the wave files
+    play_wf.close()
+    record_wf.close()
 
-                
-
-                record_wf.writeframes(data)
-            print('Done')
-
-            # Close the streams
-            record_stream.close()
-            play_stream.close()
-
-            # Release PortAudio system resources
-            p.terminate()
+    # Release PortAudio system resources
+    p.terminate()
