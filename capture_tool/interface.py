22a1ee6e59864aa4fc56e1729840657a347f432a
Ryan <ryancluf@yahoo.com>
2024-10-30 16:30:27 -0400

    add defaults to interface class

--- capture_tool/interface.py
+++ capture_tool/interface.py
@@ -4,24 +4,27 @@ import time
 import numpy as np
 import sounddevice as sd
 
-from util import SineWave, pack, unpack, v_rms_to_dbu, calculate_channel_dbfs
+from capture_tool.audio import SineWave, pack, unpack, v_rms_to_dbu, calculate_channel_dbfs
 
 
 class Interface:
     def __init__(self, config: dict):
         # Interface configuration
-        self.device = config["device"]
-        self.output_channel = config["output_channel"]
-        self.input_channels = config["input_channels"]
+        self.device = config.get("device", sd.default.device)
+        self.output_channel = config.get("output_channel", 1)
+        self.input_channels = config.get("input_channels", ["1"])
 
         # Interface calibration settings
-        self.target_dbu = config["target_dbu"]
-        self.frequency = config["frequency"]
-        self.blocksize = config["calibration_blocksize"]
-        self.samplerate = config["calibration_samplerate"]
+        self.target_dbu = config.get("target_dbu", 6.0)
+        self.frequency = config.get("frequency", 1000)
+        self.samplerate = config.get("samplerate", sd.default.samplerate)
+        self.blocksize = config.get("blocksize", self.samplerate // 100)
 
         # Interface calibration results
-        self.output_level = None
+        self.output_level = config.get("_output_level", None)
+
+    # def _print_verify_header(self):
+
 
     def calibrate(self):
         sine_wave = SineWave(self.frequency, self.samplerate, 0)
@@ -51,25 +54,21 @@ class Interface:
         )
 
         with stream:
-            reamp_v_rms = float(input(f"Enter RMS voltage: "))
+            reamp_v_rms = float(input(f"enter rms voltage: "))
             # reamp_v_rms = 3.424
             reamp_dbu = v_rms_to_dbu(reamp_v_rms)
 
-            if reamp_dbu < self.target_dbu:
-                print("Reamp gain too low, increase output level or decrease reamp pad")
-                return
+            self.output_level = self.target_dbu - reamp_dbu
 
-            output_level = self.target_dbu - reamp_dbu
-
-            sine_wave = SineWave(self.frequency, self.samplerate, output_level)
-            reamp_v_rms_verify = float(input("Enter new RMS voltage: "))
+            sine_wave = SineWave(self.frequency, self.samplerate, self.output_level)
+            reamp_v_rms_verify = float(input("enter new rms voltage: "))
             # reamp_v_rms_verify = 3.084
             reamp_dbu_verify = v_rms_to_dbu(reamp_v_rms_verify)
 
             print(f"reamp target level: {self.target_dbu:.2f} dBu")
             print(f"measured reamp level @ 0 dBFS: {reamp_dbu:.2f} dBu")
-            print(f"calculated reamp level adjustment: {output_level:.2f} dB")
-            print(f"measured reamp level @ {output_level:.2f} dB: {reamp_dbu_verify:.2f} dBu")
+            print(f"calculated reamp level adjustment: {self.output_level:.2f} dB")
+            print(f"measured reamp level @ {self.output_level:.2f} dB: {reamp_dbu_verify:.2f} dBu")
             print("")
 
             print("Verify reamp input and output levels. Press Ctrl+C to exit.")
@@ -78,13 +77,11 @@ class Interface:
             try:
                 while True:
                     input_dbfs = calculate_channel_dbfs(input_max)
-                    output_str = " | ".join(f"{dbu:.2f}" for dbu in input_dbfs)
-                    print(f"dBFS: {output_str}")
+                    output_str = " | ".join(f"{dbu:3.2f}" for dbu in input_dbfs)
+                    print(f"dBFS: {output_str}", end="\r")
                     time.sleep(1)
             except KeyboardInterrupt:
                 print("")
-
-        self.output_level = output_level
 
 if __name__ == "__main__":
     interface = Interface(
@@ -94,8 +91,8 @@ if __name__ == "__main__":
             "input_channels": ["source", "preamp", "poweramp", "di"],
             "target_dbu": 6.0,
             "frequency": 1000,
-            "calibration_blocksize": 1024,
-            "calibration_samplerate": 48000,
+            "blocksize": 1024,
+            "samplerate": 48000,
         }
     )
     interface.calibrate()
