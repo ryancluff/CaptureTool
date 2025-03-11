f8e877a8cbc29825ee6a71261b1e936b42eab3a6
Ryan <ryancluf@yahoo.com>
2025-03-11 13:46:12 -0400

    merge capture class back into audio interface. rename class to AudioInterface. remove capture_config. implement reamp and testtone functions.

--- capture_tool/cli.py
+++ capture_tool/cli.py
@@ -4,63 +4,39 @@ from pathlib import Path
 import sounddevice as sd
 import wavio
 
-from capture_tool.capture import Capture
-from capture_tool.interface import Interface
+from capture_tool.interface import AudioInterface
 from capture_tool.util import timestamp
 
 
-def _read_configs(
-    interface_config_path: Path,
-    capture_config_path: Path | None = None,
-    device_config_path: Path | None = None,
-) -> tuple[dict, dict | None, dict | None]:
-    with open(interface_config_path, "r") as fp:
-        interface_config = json.load(fp)
-        interface_config["path"] = interface_config_path
-    if capture_config_path is not None:
-        with open(capture_config_path, "r") as fp:
-            capture_config = json.load(fp)
-            capture_config["path"] = capture_config_path
-    else:
-        capture_config = None
-    if device_config_path is not None:
-        with open(device_config_path, "r") as fp:
-            device_config = json.load(fp)
-            device_config["path"] = device_config_path
-    else:
-        device_config = None
-    return interface_config, capture_config, device_config
+def _read_config(path: Path) -> dict:
+    with open(path, "r") as fp:
+        config = json.load(fp)
+        config["path"] = str(path)
+    return config
 
 
-def _write_configs(
+def _write_config(
     capture_dir: Path,
-    interface_config: dict,
-    capture_config: dict | None = None,
-    device_config: dict | None = None,
+    config: dict,
+    name: str = "interface",
 ) -> None:
-    with open(Path(capture_dir, "interface.json"), "w") as fp:
-        json.dump(interface_config, fp, indent=4)
-    if capture_config is not None:
-        with open(Path(capture_dir, "capture.json"), "w") as fp:
-            json.dump(capture_config, fp, indent=4)
-    if device_config is not None:
-        with open(Path(capture_dir, "device.json"), "w") as fp:
-            json.dump(device_config, fp, indent=4)
+    with open(Path(capture_dir, name + ".json"), "w") as fp:
+        json.dump(config, fp, indent=4)
 
 
-def create_captures_dir(path: str = "captures") -> Path:
+def _create_captures_dir(path: str = "captures") -> Path:
     captures_dir = Path(path)
     captures_dir.mkdir(exist_ok=True)
     return captures_dir
 
 
-def create_capture_dir(captures_dir: Path, path: str = timestamp()) -> Path:
+def _create_capture_dir(captures_dir: Path, path: str = timestamp()) -> Path:
     captures_dir = Path(captures_dir, path)
     captures_dir.mkdir(exist_ok=False)
     return captures_dir
 
 
-def _calibrate(interface: Interface) -> float:
+def _calibrate(interface: AudioInterface) -> tuple[float, float]:
     pause_after_calibration = False
     if interface.reamp_delta is None:
         pause_after_calibration = True
@@ -68,7 +44,7 @@ def _calibrate(interface: Interface) -> float:
         input("press enter to start reamp calibration...")
         print("calibrating reamp output...")
         reamp_delta = interface.calibrate_reamp()
-        print(f"target reamp level: {interface.target_dbu:.2f} dBu")
+        print(f"target reamp level: {interface.output_level_max_dbu:.2f} dBu")
         print(f"calculated input delta (dbu - dbfs): {interface.reamp_delta:.2f}")
         print("calibration complete")
         print(f'"reamp_delta": {interface.reamp_delta:.2f}')
@@ -99,25 +75,34 @@ def _calibrate(interface: Interface) -> float:
     return reamp_delta, input_deltas
 
 
-def _passthrough(interface: Interface) -> None:
+def _passthrough(interface: AudioInterface) -> None:
     interface.passthrough()
 
 
 def cli():
     parser = ArgumentParser(description="Capture tool")
     subparsers = parser.add_subparsers(dest="command")
+
     list_parser = subparsers.add_parser("list-interfaces")
     list_parser.add_argument("interface", nargs="?", type=int, default=None)
+
     capture_parser = subparsers.add_parser("capture", help="Run a capture")
     capture_parser.add_argument("interface_config_path", type=str)
-    capture_parser.add_argument("capture_config_path", type=str)
     capture_parser.add_argument("device_config_path", nargs="?", type=str, default=None)
+
+    reamp_parser = subparsers.add_parser("reamp", help="Loop reamp output without recording")
+    reamp_parser.add_argument("interface_config_path", type=str)
+
     passthrough_parser = subparsers.add_parser("passthrough", help="Passthrough instrument audio")
     passthrough_parser.add_argument("interface_config_path", type=str)
 
+    testtone_parser = subparsers.add_parser("testtone", help="Generate a test tone")
+    testtone_parser.add_argument("interface_config_path", type=str)
+    testtone_parser.add_argument("type", nargs="?", type=str, default="dbfs")
+
     args = parser.parse_args()
     if args.command == "list-interfaces":
-        if args.interface is not None:
+        if args.interface:
             print(sd.query_devices(args.interface))
             print("")
             samplerates = [32000, 44100, 48000, 88200, 96000, 128000, 192000]
@@ -133,47 +118,55 @@ def cli():
         else:
             print(sd.query_devices())
     elif args.command == "capture":
-        capture_config, interface_config, device_config = _read_configs(
-            args.interface_config_path,
-            args.capture_config_path,
-            args.device_config_path,
-        )
-        captures_dir = create_captures_dir()
-        capture_dir = create_capture_dir(captures_dir)
+        # read configs, create dirs
+        interface_config = _read_config(args.interface_config_path)
+        if args.device_config_path:
+            device_config = _read_config(args.device_config_path)
+        captures_dir = _create_captures_dir()
+        capture_dir = _create_capture_dir(captures_dir)
 
-        capture = Capture(capture_config, Interface(interface_config))
+        interface = AudioInterface(interface_config)
         # Run calibration if needed
-        interface_config["reamp_delta"] = _calibrate(capture.interface)
-        _write_configs(
-            capture_dir,
-            interface_config,
-            capture_config,
-            device_config,
-        )
-        raw_recording, processed_recording = capture.run(plot_latency=True)
+        interface_config["reamp_delta"] = _calibrate(interface)
+        _write_config(capture_dir, interface_config)
+        if args.device_config_path:
+            _write_config(capture_dir, device_config, "device")
+        raw_recording, processed_recording = interface.capture(plot_latency=True)
         # Save the raw recording to a single wav file
         # The number of channels in the wav file is equal to the number of input channels
         wavio.write(
             str(Path(capture_dir, "recording-raw.wav")),
             raw_recording,
-            capture.input_wav.rate,
+            interface.input_wav.rate,
             sampwidth=3,
         )
         # Save the processed recording data
         # Each channel is saved to a separate mono wav file
-        for i in range(len(capture.interface.channels["input"])):
-            channel = capture.interface.channels["input"][i]
+        for i in range(len(interface.interface.channels["input"])):
+            channel = interface.interface.channels["input"][i]
             wavio.write(
                 str(Path(capture_dir, f"recording-{channel}.wav")),
                 processed_recording[:, i],
-                capture.input_wav.rate,
+                interface.input_wav.rate,
                 sampwidth=3,
             )
+    elif args.command == "reamp":
+        interface_config = _read_config(args.interface_config_path)
+        interface = AudioInterface(interface_config)
+        interface_config["reamp_delta"], interface_config["input_deltas"] = _calibrate(interface)
     elif args.command == "passthrough":
-        interface_config = _read_configs(args.interface_config_path)
-        interface = Interface(interface_config)
+        interface_config = _read_config(args.interface_config_path)
+        interface = AudioInterface(interface_config)
         interface_config["reamp_delta"], interface_config["input_deltas"] = _calibrate(interface)
         _passthrough(interface)
+    elif args.command == "testtone":
+        interface_config = _read_config(args.interface_config_path)
+        interface = AudioInterface(interface_config)
+        if args.type == "dbfs":
+            interface.testtone()
+        elif args.type == "dbu":
+            _calibrate(interface)
+            interface.testtone_dbu()
     print("done")
 
 
--- capture_tool/interface.py
+++ capture_tool/interface.py
@@ -1,35 +1,53 @@
+from enum import Enum
 import sys
 import threading
 import time
 
+import matplotlib.pyplot as plt
 import numpy as np
 import sounddevice as sd
+import wavio
 
 from capture_tool.audio import (
     SineWave,
     pack,
     unpack,
     v_rms_to_dbu,
+    db_to_scalar,
     dbu_to_dbfs,
     dbfs_to_dbu,
     int_to_dbfs,
 )
 
+LATENCY_OFFSET = 0
 
-class Interface:
+
+class LatencyAdjustment(Enum):
+    NONE = 0
+    BASE = 1
+    INDIVIDUAL = 2
+
+
+class TestToneMode(Enum):
+    DBFS = 0
+    DBU = 1
+
+
+class AudioInterface:
     def __init__(self, config: dict):
+        self.input_wav = wavio.read(config["reamp_file"])
+        self.send_level_dbu = config["reamp_level_dbu"]
+
         # Interface configuration
         self.device = config.get("device", sd.default.device)
+        self.samplerate = config.get("samplerate", self.input_wav.rate)
+        self.blocksize = config.get("blocksize", 256)
         self.channels = config.get("channels", {})
         self.num_output_channels = self.channels["reamp"]
         self.num_input_channels = len(self.channels["input"])
 
-        # Interface settings
-        self.samplerate = config.get("samplerate", sd.default.samplerate)
-        self.blocksize = config.get("blocksize", 256)
-
         # Calibration settings
-        self.target_dbu = config.get("target_dbu", 6.0)
+        self.output_level_max_dbu = config.get("output_level_max_dbu", 6.0)
         self.frequency = config.get("frequency", 1000)
 
         # Interface calibration results
@@ -75,14 +93,14 @@ class Interface:
             reamp_v_rms = float(input(f"enter measured rms voltage: "))
             reamp_dbu = v_rms_to_dbu(reamp_v_rms)
             self.reamp_delta = reamp_dbu - init_dbfs
-            target_dbfs = dbu_to_dbfs(self.target_dbu, self.reamp_delta)
+            target_dbfs = dbu_to_dbfs(self.output_level_max_dbu, self.reamp_delta)
 
-            while reamp_dbu - self.target_dbu > 0.001:
+            while reamp_dbu - self.output_level_max_dbu > 0.001:
                 sine_wave = SineWave(self.frequency, self.samplerate, target_dbfs)
                 reamp_v_rms = float(input("enter new rms voltage: "))
                 reamp_dbu = v_rms_to_dbu(reamp_v_rms)
                 self.reamp_delta = reamp_dbu - init_dbfs
-                target_dbfs = dbu_to_dbfs(self.target_dbu, self.reamp_delta)
+                target_dbfs = dbu_to_dbfs(self.output_level_max_dbu, self.reamp_delta)
 
         return self.reamp_delta
 
@@ -141,6 +159,265 @@ class Interface:
 
         return self.input_deltas
 
+    def _calculate_latency(
+        self,
+        reamp_data: np.ndarray,
+        raw_recording: np.ndarray,
+        cc_len: int = 5,
+    ) -> tuple[np.ndarray, np.ndarray]:
+        channel_delays = np.zeros(self.num_input_channels, dtype=np.int32)
+        channel_inversions = np.zeros(self.num_input_channels, dtype=np.bool)
+
+        # trim input and output data for cross correlation
+        reamp_short = reamp_data[: self.input_wav.rate * cc_len, 0]
+        recording_short = raw_recording[: self.input_wav.rate * cc_len, :]
+
+        # normalize data to -1 to 1 to prevent overflow in cross correlation
+        reamp_short = reamp_short / np.max(np.abs(reamp_short))
+        for i in range(self.num_input_channels):
+            output_data_short_normalized = recording_short[:, i] / np.max(np.abs(recording_short[:, i]))
+
+            # calculate cross correlation for each channel
+            # if the maximum cross correlation is negative, invert the channel
+            cross_corr = np.correlate(reamp_short, output_data_short_normalized, mode="full")
+            max_cc = np.argmax(cross_corr)
+            min_cc = np.argmin(cross_corr)
+            if np.abs(cross_corr[max_cc]) < np.abs(cross_corr[min_cc]):
+                max_cc = min_cc
+                channel_inversions[i] = True
+
+            # Calculate the delay for each channel
+            channel_delays[i] = len(recording_short) - max_cc - 1 - LATENCY_OFFSET
+        return channel_delays, channel_inversions
+
+    def _process_recordings(
+        self,
+        reamp_audio: np.ndarray,
+        raw_recording: np.ndarray,
+        channel_delays: np.ndarray,
+        channel_inversions: np.ndarray,
+        latency_adjustment: LatencyAdjustment = LatencyAdjustment.BASE,
+        inversion_adjustment: bool = True,
+    ) -> np.ndarray:
+        processed_recording = np.zeros_like(raw_recording)
+
+        # apply the calculated delays to the recording data
+        if latency_adjustment == LatencyAdjustment.BASE:
+            for i in range(self.num_input_channels):
+                processed_recording[: -channel_delays[i], i] = raw_recording[channel_delays[i] :, i]
+        elif latency_adjustment == LatencyAdjustment.INDIVIDUAL:
+            for i in range(self.num_input_channels):
+                processed_recording[: -channel_delays[0], i] = raw_recording[channel_delays[0] :, i]
+        else:
+            processed_recording[:, :] = raw_recording[:, :]
+
+        # invert the recording data if necessary
+        if inversion_adjustment:
+            for i in range(self.num_input_channels):
+                if channel_inversions[i]:
+                    print(f"detected signal inversion on channel {i}, correcting")
+                    processed_recording[:, i] *= -1
+
+        # trim the recording data to the length of the reamp data
+        processed_recording = processed_recording[: len(reamp_audio), :]
+
+        return processed_recording
+
+    def capture(
+        self,
+        plot_latency: bool = False,
+        latency_adjustment: LatencyAdjustment = LatencyAdjustment.BASE,
+        inversion_adjustment: bool = True,
+    ) -> tuple[np.ndarray, np.ndarray]:
+        if self.reamp_delta is None:
+            raise RuntimeError("reamp not calibrated. exitting...")
+
+        # scale the reamp data using the reamp delta to output at the proper level
+        reamp_audio = np.array(self.input_wav.data * db_to_scalar(0 - self.reamp_delta), dtype=np.int32)
+        # append 10 blocks of zeros to the end of the input data to account for latency
+        raw_recording = np.zeros((len(reamp_audio) + 10 * self.blocksize, self.num_input_channels), dtype=np.int32)
+
+        current_frame = 0
+        recording_levels = np.zeros(self.num_input_channels, dtype=np.int32)
+        recording_done = threading.Event()
+
+        def callback(indata, outdata, frames, time, status):
+            if status:
+                print(status, file=sys.stderr)
+
+            nonlocal recording_levels
+            nonlocal current_frame
+            chunksize = min(len(reamp_audio) - current_frame, frames)
+
+            # write the reamp data to the interface output channel
+            output = np.zeros((frames, self.num_output_channels))
+            output[:chunksize, self.channels["reamp"] - 1] = reamp_audio[
+                current_frame : current_frame + chunksize
+            ].flatten()
+            outdata[:] = pack(output)
+
+            # read the recording data from the interface input channels
+            input = unpack(indata, self.num_input_channels)
+            raw_recording[current_frame : current_frame + frames] = input
+
+            recording_levels[:] = np.max(np.abs(input), axis=0)
+            current_frame += frames
+            if current_frame >= len(reamp_audio):
+                raise sd.CallbackStop()
+
+        stream = sd.RawStream(
+            samplerate=self.input_wav.rate,
+            blocksize=self.blocksize,
+            device=self.device,
+            channels=(self.num_input_channels, self.num_output_channels),
+            dtype="int24",
+            callback=callback,
+            finished_callback=recording_done.set,
+        )
+
+        peak_levels = np.zeros(self.num_input_channels, dtype=np.int32)
+        with stream:
+            while not recording_done.wait(timeout=1.0):
+                for i in range(self.num_input_channels):
+                    if recording_levels[i] > peak_levels[i]:
+                        peak_levels[i] = recording_levels[i]
+                peak_dbfs = int_to_dbfs(peak_levels)
+                output_str = " | ".join(f"{dbu:3.2f}" for dbu in peak_dbfs)
+                current_seconds = current_frame // self.input_wav.rate
+                current_seconds = f"{current_seconds // 60:02d}:{current_seconds % 60:02d}"
+                total_seconds = len(self.input_wav.data) // self.input_wav.rate
+                total_seconds = f"{total_seconds // 60:02d}:{total_seconds % 60:02d}"
+                print(f"{current_seconds} / {total_seconds} - {output_str}          ")
+
+        channel_delays, channel_inversions = self._calculate_latency(reamp_audio, raw_recording)
+
+        processed_recording = self._process_recordings(
+            reamp_audio,
+            raw_recording,
+            channel_delays,
+            channel_inversions,
+            inversion_adjustment=inversion_adjustment,
+            latency_adjustment=latency_adjustment,
+        )
+
+        if plot_latency:
+            for i in range(self.num_input_channels):
+                samples = self.input_wav.rate * 5
+                plt.figure(figsize=(16, 5))
+                plt.plot(
+                    reamp_audio[:samples] / np.max(reamp_audio[:samples]),
+                    label="reamp",
+                )
+                plt.plot(
+                    raw_recording[:samples, i] / np.max(raw_recording[:samples, i]),
+                    linestyle="--",
+                    label="raw recording",
+                )
+                plt.plot(
+                    processed_recording[:samples, i] / np.max(processed_recording[:samples, i]),
+                    linestyle="-.",
+                    label="processed recording",
+                )
+                plt.title(
+                    f"channel={i} | base delay={channel_delays[0]} | channel_delay={channel_delays[i]} | invert={channel_inversions[i]}"
+                )
+                plt.legend()
+                plt.show(block=True)
+
+        return raw_recording, processed_recording
+
+    def testtone(self, mode: TestToneMode = TestToneMode.DBFS):
+        if mode == TestToneMode.DBU and self.reamp_delta is None:
+            raise RuntimeError("reamp not calibrated. exitting...")
+
+        num_output_channels = self.channels["reamp"]
+
+        output_level = -3
+        sine_wave = SineWave(self.frequency, self.samplerate, output_level)
+
+        def callback(outdata, frames, time, status):
+            if status:
+                print(status, file=sys.stderr)
+
+            output = np.zeros((frames, num_output_channels), dtype=np.int32)
+            for i in range(frames):
+                output[i, self.channels["reamp"] - 1] = next(sine_wave)
+            outdata[:] = pack(output)
+
+        stream = sd.RawOutputStream(
+            samplerate=self.samplerate,
+            blocksize=self.blocksize,
+            device=self.device,
+            channels=num_output_channels,
+            dtype="int24",
+            callback=callback,
+        )
+
+        try:
+            with stream:
+                print("press ctrl+c to stop")
+                print("enter 1 to increase output level, 2 to decrease output level")
+                while True:
+                    print("output level (dbfs): ", output_level)
+                    control = input("> ")
+                    if control == "1":
+                        output_level += 1
+                        sine_wave = SineWave(self.frequency, self.samplerate, output_level)
+                    elif control == "2":
+                        output_level -= 1
+                        sine_wave = SineWave(self.frequency, self.samplerate, output_level)
+                    else:
+                        print("invalid input")
+
+        except KeyboardInterrupt:
+            pass
+
+    def reamp(self):
+        if self.reamp_delta is None:
+            raise RuntimeError("reamp not calibrated. exitting...")
+
+        # scale the reamp data using the reamp delta to output at the proper level
+        reamp_audio = np.array(self.input_wav.data * db_to_scalar(0 - self.reamp_delta), dtype=np.int32)
+
+        current_frame = 0
+        recording_done = threading.Event()
+
+        def callback(outdata, frames, time, status):
+            if status:
+                print(status, file=sys.stderr)
+
+            nonlocal current_frame
+            chunksize = min(len(reamp_audio) - current_frame, frames)
+
+            # write the reamp data to the interface output channel
+            output = np.zeros((frames, self.num_output_channels))
+            output[:chunksize, self.channels["reamp"] - 1] = reamp_audio[
+                current_frame : current_frame + chunksize
+            ].flatten()
+            outdata[:] = pack(output)
+
+            current_frame += frames
+            if current_frame >= len(reamp_audio):
+                raise sd.CallbackStop()
+
+        stream = sd.RawOutputStream(
+            samplerate=self.input_wav.rate,
+            blocksize=self.blocksize,
+            device=self.device,
+            channels=(self.num_input_channels, self.num_output_channels),
+            dtype="int24",
+            callback=callback,
+            finished_callback=recording_done.set,
+        )
+
+        with stream:
+            while not recording_done.wait(timeout=1.0):
+                current_seconds = current_frame // self.input_wav.rate
+                current_seconds = f"{current_seconds // 60:02d}:{current_seconds % 60:02d}"
+                total_seconds = len(self.input_wav.data) // self.input_wav.rate
+                total_seconds = f"{total_seconds // 60:02d}:{total_seconds % 60:02d}"
+                print(f"{current_seconds} / {total_seconds}")
+
     def passthrough(self):
         def callback(indata, outdata, frames, time, status):
             if status:
@@ -157,5 +434,10 @@ class Interface:
             callback=callback,
         )
 
-        with stream:
-            time.sleep(duration)
+        try:
+            with stream:
+                print("press ctrl+c to stop")
+                while True:
+                    time.sleep(1)
+        except KeyboardInterrupt:
+            pass
