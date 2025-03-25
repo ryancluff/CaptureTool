730de39666c991d2ee13cb6092b1622426e1805a
Ryan <ryancluf@yahoo.com>
2025-03-25 15:08:18 -0400

    move dbu <-> dbfs conversions to interface class. rename input_max -> max_val param for int_to_dbfs func

--- capture_tool/audio.py
+++ capture_tool/audio.py
@@ -82,16 +82,6 @@ def dbu_to_v_rms(dbu: float) -> float:
     return 0.7746 * 10 ** (dbu / 20)
 
 
-# Convert dBu to dBFS
-def dbu_to_dbfs(dbu: float, delta: float) -> float:
-    return dbu - delta
-
-
-# Convert dBFS to dBu
-def dbfs_to_dbu(dbfs: float, delta: float) -> float:
-    return dbfs + delta
-
-
 # Convert 24 bit audio data to dBFS
-def int_to_dbfs(input_max: np.array) -> np.array:
-    return 20 * np.log10(input_max / (MAX_VAL_INT24))
+def int_to_dbfs(max_val: np.array) -> np.array:
+    return 20 * np.log10(max_val / (MAX_VAL_INT24))
