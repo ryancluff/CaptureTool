2324687459467ec4743e4127f824ff4bd03e2770
Ryan <ryancluf@yahoo.com>
2025-05-05 20:31:27 -0400

    add sine sweep script

---
+++ scripts/generate_sine_sweep.py
@@ -0,0 +1,22 @@
+import matplotlib.pyplot as plt
+import wavio
+
+from capture_tool.wave import SweepWave
+
+
+if __name__ == "__main__":
+    start_freq = 20
+    end_freq = 20000
+    duration = 5
+    sample_rate = 48000
+    dbfs = -0.1
+
+    w = SweepWave(
+        start_freq=start_freq,
+        end_freq=end_freq,
+        duration=duration,
+        samplerate=sample_rate,
+        dbfs=dbfs,
+        loop=False,
+    )
+    wavio.write("sine_sweep.wav", w.lookup_table, sample_rate, sampwidth=3)
