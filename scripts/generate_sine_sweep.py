07ddb1e19c455bde7e119bb27b2fdbf45e5908a0
Ryan Cluff <ryancluf@yahoo.com>
2025-06-14 19:28:38 -0400

    remove explicit var names, cahgne sweep values

--- scripts/generate_sine_sweep.py
+++ scripts/generate_sine_sweep.py
@@ -7,16 +7,15 @@ from capture_tool.wave import SweepWave
 if __name__ == "__main__":
     start_freq = 20
     end_freq = 20000
-    duration = 5
+    duration = 10
     sample_rate = 48000
-    dbfs = -0.1
+    dbfs = 0
 
     w = SweepWave(
-        start_freq=start_freq,
-        end_freq=end_freq,
-        duration=duration,
-        samplerate=sample_rate,
-        dbfs=dbfs,
-        loop=False,
+        start_freq,
+        end_freq,
+        duration,
+        sample_rate,
+        dbfs
     )
     wavio.write("sine_sweep.wav", w.audio, sample_rate, sampwidth=3)
